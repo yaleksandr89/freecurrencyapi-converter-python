8675ed85528bd3ed27b8d5b0be54d6f5482cc39b
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2024-11-18 14:12:31 +0300

    import 'pprint' and modified test.py

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,28 @@
 import os
 import requests
+from pprint import pprint
 from dotenv import load_dotenv
 
-load_dotenv()
-api_token = os.getenv('API_TOKEN')
+
+def get_environment_variable(var_name):
+    """
+    Получает переменную окружения. Выбрасывает исключение, если переменной нет.
+    """
+    value = os.getenv(var_name)
+    if not value:
+        raise ValueError(f"Отсутствует обязательная переменная окружения: {var_name}")
+    return value
+
+
+# Глобальные переменные для API
+API_TOKEN = get_environment_variable("API_TOKEN")
+API_URL = get_environment_variable("API_URL")
 
 
 def get_online_currencies():
-    all_currencies_url = f'https://api.freecurrencyapi.com/v1/currencies?apikey={api_token}&currencies='
-    response = requests.get(all_currencies_url)
-    print(response.json().get('data'))
-    return response.json().get('data')
+    currencies_endpoint = f"{API_URL}/currencies?apikey={API_TOKEN}"
+    response = requests.get(currencies_endpoint)
+    pprint(response.json().get('data'))
 
 
 get_online_currencies()
